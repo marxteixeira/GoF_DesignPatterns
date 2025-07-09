34bb870d6b822e38c0dc2161f82d0d491a28a4d5
marxteixeira <marxtts@gmail.com>
2025-07-08 22:36:20 -0300

    Esboço inicial dos arquivos SocorroVeiculoFactory.

---
+++ src/DesignPatternProject/01-Creational/1.1-Abstract Factory/SocorroVeiculoGrandeFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternProject._01_Creational._1._1_Abstract_Factory
+{
+    internal class SocorroVeiculoGrandeFactory
+    {
+    }
+}
---
+++ src/DesignPatternProject/01-Creational/1.1-Abstract Factory/SocorroVeiculoPequenoFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternProject._01_Creational._1._1_Abstract_Factory
+{
+    // Concrete Factory
+    public class SocorroVeiculoPequenoFactory : AutoSocorroFactory
+    {
+        public override Guincho CriarGuincho()
+        {
+            return GuinchoCreator.Criar(Porte.Pequeno);
+        }
+
+        public override Veiculo CriarVeiculo(string modelo, Porte porte)
+        {
+            return VeiculoCreator.Criar(modelo, porte);
+        }
+    }
+}
