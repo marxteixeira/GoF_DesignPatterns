610832e23170182d8c02644f19727ab7f83a9500
marxteixeira <marxtts@gmail.com>
2025-07-08 22:16:43 -0300

    AutoSocorro e AutoSocorroFactory

---
+++ src/DesignPatternProject/01-Creational/1.1-Abstract Factory/AutoSocorroFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternProject._01_Creational._1._1_Abstract_Factory
+{
+    // Abstract Factory
+    public abstract class AutoSocorroFactory
+    {
+        public abstract Guincho CriarGuincho();
+        public abstract Veiculo CriarVeiculo(string modelo, Porte porte);
+    }
+}
